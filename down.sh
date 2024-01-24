bdf8fcf7b48033f4c8fdba6c57b01934db291327
Cyber Anonymous <cyberx99@disroot.org>
2024-01-24 01:08:18 -0300

    fix down and added key.txt

--- down.sh
+++ down.sh
@@ -1,3 +1,4 @@
 url='https://raw.githubusercontent.com/Cyberx9901/mygames/main'
 export GPG_TTY=$(tty)
-curl -L "$url/$(cat $url/list.txt | fzf )" | gpg -a -d --batch --passphrase $(cat key.txt)
+file=$(curl -L "$url/list.txt" | fzf)
+curl -L "$url/$(echo $file | sed s/" "/"%20"/g)" | gpg -a -d --batch --passphrase $(cat key.txt) -o "$(echo "$file" | sed s/"\.asc"/""/g)"
